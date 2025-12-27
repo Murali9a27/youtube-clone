874e6e6b13ea0fd68386855f2e263c77029cf70e
Murali9a27 <murali9a27@gmail.com>
2025-12-27 23:40:05 +0530

    controler Updated

--- Backend/src/controllers/user.controller.js
+++ Backend/src/controllers/user.controller.js
@@ -255,7 +255,7 @@ const getCurrentUser = asyncHandler((req,res)=>{
 
 const updateUserDetails = asyncHandler(async(res, res)=>{
     const {fullname, email} = req.body
-    const user = User.findByIdAndUpdate(
+    const user = await User.findByIdAndUpdate(
         req.user._id,
         {
             fullname: fullname,
@@ -266,5 +266,17 @@ const updateUserDetails = asyncHandler(async(res, res)=>{
             runValidators: true
         }
     )
+
+    if(!user){
+        throw new ApiError(401, "User not found")
+    }
 })
-export { registerUser, loginUser, logoutUser, refreshAccessToken, changeUserPassword, getCurrentUser };
+export { 
+    registerUser, 
+    loginUser, 
+    logoutUser, 
+    refreshAccessToken, 
+    changeUserPassword, 
+    getCurrentUser, 
+    updateUserDetails 
+};
