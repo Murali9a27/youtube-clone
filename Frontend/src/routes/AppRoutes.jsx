6b252a7c8bbb69daaf0d755771bb42a5f071abf6
Murali9a27 <murali9a27@gmail.com>
2026-01-14 22:21:33 +0530

    created route files

---
+++ Frontend/src/routes/AppRoutes.jsx
@@ -0,0 +1,16 @@
+import { Routes, Route } from "react-router-dom";
+import Login from "../pages/auth/Login.jsx";
+import Register from "../pages/auth/Register";
+import Home from "../pages/Home";
+
+const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+    </Routes>
+  );
+};
+
+export default AppRoutes;
