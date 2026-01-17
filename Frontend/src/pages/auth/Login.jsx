48ddf8df74c2f43c6ed9906087e78d3bc8620046
Murali9a27 <murali9a27@gmail.com>
2026-01-17 23:51:50 +0530

    style update

--- Frontend/src/pages/auth/Login.jsx
+++ Frontend/src/pages/auth/Login.jsx
@@ -39,13 +39,13 @@ const Login = () => {
               onChange={handleChange}
               placeholder="you@example.com"
               className="w-full px-4 py-2 border border-gray-200 rounded-lg 
-              focus:outline-none focus:border-red-100"
+              focus:outline-none focus:border-red-300"
               required
             />
           </div>
 
           <div>
-            <label className="block text-sm font-medium text-gray-700 mb-1">
+            <label className="block text-sm font-medium text-gray-900 mb-1">
               Password
             </label>
             <input
@@ -54,8 +54,8 @@ const Login = () => {
               value={formData.password}
               onChange={handleChange}
               placeholder="••••••••"
-              className="w-full px-4 py-2 border border-gray-300 rounded-lg 
-              focus:outline-none focus:ring-2  focus:border-red-500"
+              className="w-full px-4 py-2 border border-gray-200 rounded-lg 
+              focus:outline-none focus:ring-2  focus:border-red-300"
               required
             />
           </div>
